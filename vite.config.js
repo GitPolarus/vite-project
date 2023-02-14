86702ac62e6ff0249107327446b1afd31ddc1256
Hippolyte EZIAN <polarusdev@gmail.com>
2023-02-14 16:00:20 +0000

    to deploy.

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-    "scripts": {
-      "build": "vite build",
-      "preview": "vite preview"
-    },
-  "base" : "/vite-project/"
-  
+  base:"/vite-project"
 })
